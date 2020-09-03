929275a8aac531ca823dc3cca9d347ccd8d13adb
Ahmed <ahmedjomer@gmail.com>
2020-09-02 22:04:18 -0700

    footer styling

--- grotto/src/components/pagelayout/Footer/Footer.js
+++ grotto/src/components/pagelayout/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 // CSS \\
+import "./Footer.css";
 
 const Footer = () => {
     return ( <
@@ -22,6 +23,17 @@ const Footer = () => {
         className = "fa fa-facebook" /
         >
         <
+        /div>{" "} <
+        div className = "footer-address" >
+        <
+        p >
+        Old Fisherman 's Grotto, 39 Fisherman'
+        s Wharf, Monterey, California 93940 { " " } <
+        /p>{" "} <
+        /div>{" "} <
+        div className = "footer-copyright" >
+        <
+        p > ©2020 Old Fisherman 's Grotto</p>{" "} <
         /div>{" "} <
         /div>{" "} <
         /div>
